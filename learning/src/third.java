55cb117d1c89b51421b64eac3a808e5c1722d95f
samli01 <samim1999bim@gmail.com>
2026-02-03 23:15:08 +0545

    updated version of else-if

--- learning/src/third.java
+++ learning/src/third.java
@@ -1,13 +1,15 @@
 public class third {
     public static void main(String[] args) {
         int a = 5;
-        int b = 5;
+        int b = 6;
         int sum = a + b;
 
         if (sum == 10){
             System.out.println("Thanks for playing!");
-        }
-        else{
+        } else if (sum >10) {
+            System.out.println("thanks for coming samim");
+
+        } else{
             System.out.println("Thanks for joining");
         }
 
