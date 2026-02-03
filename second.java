1d9a52892c9680eb26e93b3cd3320dfd7357e741
samli01 <samim1999bim@gmail.com>
2026-02-03 22:33:36 +0545

    second program

---
+++ second.java
@@ -0,0 +1,8 @@
+public class second {
+    public static void main(String[] args) {
+        int apple = 5;
+        int banana = 5;
+        int total = apple + banana;
+        System.out.println("The total number of fruits are:" + total);
+    }
+}
