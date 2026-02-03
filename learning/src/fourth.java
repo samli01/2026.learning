73a3e89385c4b83a8ccdec17326b00dd05c1e946
samli01 <samim1999bim@gmail.com>
2026-02-03 23:59:49 +0545

    this is methods

---
+++ learning/src/fourth.java
@@ -0,0 +1,10 @@
+public class fourth {
+    public void  fourth() {
+        int age = 18;
+        if (age >= 18) System.out.println("eligible for voting");
+    }
+    public static void main(String[] args) {
+        fourth f = new fourth();
+        f.fourth();
+    }
+}
