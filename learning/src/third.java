0481b28f4b45829937e6ed8a8f4b0eb4f66efea3
samli01 <samim1999bim@gmail.com>
2026-02-03 23:09:06 +0545

    this is the third program

---
+++ learning/src/third.java
@@ -0,0 +1,16 @@
+public class third {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int sum = a + b;
+
+        if (sum == 10){
+            System.out.println("Thanks for playing!");
+        }
+        else{
+            System.out.println("Thanks for joining");
+        }
+
+    }
+
+}
