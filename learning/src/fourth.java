0c1e8a7c12756d5c07704cb7f96ba4b37fae161e
samli01 <samim1999bim@gmail.com>
2026-02-08 22:38:48 +0545

    update in methods

--- learning/src/fourth.java
+++ learning/src/fourth.java
@@ -3,8 +3,16 @@ public class fourth {
         int age = 18;
         if (age >= 18) System.out.println("eligible for voting");
     }
+    public void voter() {
+        int age1 = 17;
+        if (age1 >= 18) System.out.println("eligible for voting");
+        else System.out.println(" this is method voter = Not eligible for voting");
+    }
     public static void main(String[] args) {
         fourth f = new fourth();
         f.fourth();
+        f.voter();
     }
+
 }
+
